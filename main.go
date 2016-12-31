a3f46d520fe53d32044363b0a3a2f23dd96a78c2
qianlifu12345 <qianlifu12345@126.com>
2016-12-31 11:50:15 +0800

    fix some bug

--- main.go
+++ main.go
@@ -2,19 +2,49 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/rancher/rancher-auth-filter-service/manager"
 	"github.com/rancher/rancher-auth-filter-service/service"
+	"github.com/urfave/cli"
 )
 
 var VERSION = "v0.1.0-dev"
 
 func main() {
-	manager.GetCommand()
 	logrus.Infof("Starting authantication filtering Service")
-	router := service.NewRouter()
-	http.Handle("/", router)
-	serverString := ":" + manager.Port
-	logrus.Fatal(http.ListenAndServe(serverString, nil))
+	//init parsing command line
+	app := cli.NewApp()
+	app.Name = "rancher-auth-filter-service"
+	app.Version = "v0.1.0-dev"
+	app.Usage = "Rancher authantication Filter Service"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "rancherUrl",
+			Value: "http://54.255.182.226:8080/",
+			Usage: "Rancher server url",
+		},
+		cli.StringFlag{
+			Name:  "localport",
+			Value: "8080",
+			Usage: "Local server port ",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		manager.Url = c.String("rancherUrl")
+		manager.Port = c.String("localport")
+		logrus.Infof("URL:" + manager.Url + "LocalPort:" + manager.Port)
+		//create mux router
+		router := service.NewRouter()
+		http.Handle("/", router)
+		serverString := ":" + manager.Port
+		//start local server
+		logrus.Fatal(http.ListenAndServe(serverString, nil))
+		return nil
+	}
+
+	app.Run(os.Args)
+
 }
--- manager/authfilter.go
+++ manager/authfilter.go
@@ -1,45 +1,4 @@
 package manager
 
-import (
-	"os"
-
-	"fmt"
-
-	"github.com/Sirupsen/logrus"
-	"github.com/urfave/cli"
-)
-
 var Url = "http://54.255.182.226:8080/"
 var Port = "8080"
-
-//GetCommand is to get the rancher server url and local port
-func GetCommand() {
-	app := cli.NewApp()
-	app.Name = "rancher-auth-filter-service"
-	app.Version = "v0.1.0-dev"
-	app.Usage = "Rancher authantication Filter Service"
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "rancherUrl",
-			Value: "http://54.255.182.226:8080/",
-			Usage: "Rancher server url",
-		},
-		cli.StringFlag{
-			Name:  "localport",
-			Value: "8080",
-			Usage: "Local server port ",
-		},
-	}
-	app.Action = func(c *cli.Context) error {
-		Url = c.String("rancherUrl")
-		Port = c.String("localport")
-		logrus.Infof("URL:" + Url + "LocalPort:" + Port)
-		// logrus.Infof(c.NArg)
-		fmt.Println(c.NArg())
-		return nil
-	}
-
-	app.Run(os.Args)
-
-}
